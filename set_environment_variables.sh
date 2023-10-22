5eca4d04d3106c446cd53cce4244ed94ca4d643d
Vinicius Benevides <massaki1999@gmail.com>
2023-10-22 03:13:17 -0300

    fix /etc/environment variables

--- set_environment_variables.sh
+++ set_environment_variables.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo echo "export MOZ_ENABLE_WAYLAND=1 # for firefox to run on wayland
-export MOZ_WEBRENDER=1
-export WLR_NO_HARDWARE_CURSORS=1
-export XDG_CURRENT_DESKTOP=sway # xdg-desktop-portal
-export XDG_SESSION_DESKTOP=sway # systemd
-export XDG_SESSION_TYPE=wayland # xdg/systemd
+sudo echo "MOZ_ENABLE_WAYLAND=1 # for firefox to run on wayland
+MOZ_WEBRENDER=1
+WLR_NO_HARDWARE_CURSORS=1
+XDG_CURRENT_DESKTOP=sway # xdg-desktop-portal
+XDG_SESSION_DESKTOP=sway # systemd
+XDG_SESSION_TYPE=wayland # xdg/systemd
 " >> /etc/environment
