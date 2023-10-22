0e4f1bc2fc54fb3f32094b38ebc89becf6ed77b3
WeirdThing <wdthing.1@gmail.com>
2023-10-22 01:07:11 -0300

    add install script for packages

---
+++ install_packages.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yes | yay -Syu swaylock-effects swaybg swayidle sway xorg-xwayland neofetch grim slurp wlogout flatpak mpv imv thunar ranger kitty wofi btop mako neovim zsh corectrl 
