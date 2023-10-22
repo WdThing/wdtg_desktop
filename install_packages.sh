13a4dd51f093455598279e80a8e01cfbd7287a4d
WeirdThing <wdthing.1@gmail.com>
2023-10-22 14:31:51 -0300

    add configs for wdtg's system

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-yes | yay -Syu swaylock-effects swaybg swayidle sway xorg-xwayland neofetch grim slurp wlogout flatpak mpv imv thunar ranger kitty wofi btop mako neovim zsh corectrl waybar noto-fonts-emoji ttf-meslo-nerd-font-powerlevel10k otf-font-awesome nwg-look-bin wl-clipboard lm_sensors
+yes | yay -Syu swaylock-effects swaybg swayidle sway xorg-xwayland neofetch grim slurp wlogout flatpak mpv imv thunar ranger kitty wofi btop mako neovim zsh corectrl waybar noto-fonts-emoji ttf-meslo-nerd-font-powerlevel10k otf-font-awesome nwg-look-bin wl-clipboard lm_sensors polkit-gnome mangohud gamemode gnome-disk-utility gparted steam-devices-git noise-suppression-for-voice
