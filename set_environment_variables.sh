5c49efe355c94b15b3d6889469d88c19e094cc7f
Vinicius Benevides <massaki1999@gmail.com>
2023-10-22 02:58:47 -0300

    system wide variables break the system

--- set_environment_variables.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo echo "export MOZ_ENABLE_WAYLAND=1 # for firefox to run on wayland
-export MOZ_WEBRENDER=1
-export WLR_NO_HARDWARE_CURSORS=1
-export XDG_CURRENT_DESKTOP=sway # xdg-desktop-portal
-export XDG_SESSION_DESKTOP=sway # systemd
-export XDG_SESSION_TYPE=wayland # xdg/systemd
-" >> /etc/environment
