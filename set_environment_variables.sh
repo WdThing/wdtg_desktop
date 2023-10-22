6358f88e9fe96aac499e979f3bc31b3606641e84
Vinicius Benevides <massaki1999@gmail.com>
2023-10-22 03:34:03 -0300

    remove /etc/environment and add lm_sensors

--- set_environment_variables.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo echo "MOZ_ENABLE_WAYLAND=1 # for firefox to run on wayland
-MOZ_WEBRENDER=1
-WLR_NO_HARDWARE_CURSORS=1
-XDG_CURRENT_DESKTOP=sway # xdg-desktop-portal
-XDG_SESSION_DESKTOP=sway # systemd
-XDG_SESSION_TYPE=wayland # xdg/systemd
-" >> /etc/environment
