7bcbd2d014ae7dce42254df763f00ba25a26274a
wdthing <wdthing.1@gmail.com>
2025-04-08 11:44:03 -0300

    include sudo

--- install_wm.sh
+++ install_wm.sh
@@ -12,26 +12,26 @@ fi
 project_dir=$(dirname "$0")
 
 # Install base packages
-${project_dir}/scripts/core_packages.sh
+sudo ${project_dir}/scripts/core_packages.sh
 
 # Install WM specific packages
 if [[ $1 == "i3" ]]; then
-    ${project_dir}/scripts/i3_packages.sh
+    sudo ${project_dir}/scripts/i3_packages.sh
 elif [[ $1 == "sway" ]]; then
-    ${project_dir}/scripts/sway_packages.sh
+    sudo ${project_dir}/scripts/sway_packages.sh
 fi
 
 # Move home
-${project_dir}/scripts/copy_to_home.sh ${project_dir}
+sudo ${project_dir}/scripts/copy_to_home.sh ${project_dir}
 
 # Disable page
-${project_dir}/scripts/disable_page.sh
+sudo ${project_dir}/scripts/disable_page.sh
 
 # Include colemak layout
-${project_dir}/extra/keyboard/install_colemak.sh
+sudo ${project_dir}/extra/keyboard/install_colemak.sh
 
 # Enable zram
-${project_dir}/extra/zram/install_zram.sh
+sudo ${project_dir}/extra/zram/install_zram.sh
 
 echo "__________________________________________________"
 echo "Finished installation, please reboot"
