40feeb1731c03ef18653fbb5964cbf666d7968df
wdthing <wdthing.1@gmail.com>
2025-04-08 10:08:11 -0300

    fix bug due to shell changing mid install

--- scripts/base_packages.sh
+++ scripts/base_packages.sh
@@ -65,7 +65,6 @@ winetricks
 "
 font_packages="noto-fonts-emoji
 otf-font-awesome
-ttf-font-awesome
 ttf-liberation
 ttf-meslo-nerd-font-powerlevel10k
 "
@@ -98,7 +97,7 @@ echo "__________________________________________________"
 echo "Installing oh-my-zsh"
 echo "__________________________________________________"
 rm -rf $HOME/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
