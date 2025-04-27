8d12be6cf9e3ef3ce4df0ca30424a2cfd0390d0c
wdthing <wdthing.1@gmail.com>
2025-04-27 11:03:47 -0300

    fix typo

--- update_project.sh
+++ update_project.sh
@@ -20,7 +20,7 @@ tracked_dirs["$HOME/.config/picom"]="./home/.config"
 tracked_dirs["$HOME/.config/wireplumber"]="./home/.config"
 tracked_dirs["$HOME/.config/redshift.conf"]="./home/.config"
 tracked_dirs["$HOME/.config/hypr"]="./home/.config"
-tracked_dirs["$HOME/.config/easyeffects"]="./hoime/.config"
+tracked_dirs["$HOME/.config/easyeffects"]="./home/.config"
 
 # Dirs to be copied to .local/share
 tracked_dirs["$HOME/.local/share/nwg-look"]="./home/.local/share"
