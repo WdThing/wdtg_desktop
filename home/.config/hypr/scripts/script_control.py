fb53cba871d2204aa5df06f8d7cd75c052b5f05d
wdthing <wdthing.1@gmail.com>
2025-05-09 07:54:08 -0300

    [Hyprland] Include python script to control the dispatch of scripts

---
+++ home/.config/hypr/scripts/script_control.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import datetime
+import time
+import subprocess
+
+if __name__ == "__main__":
+    while True:
+        now = datetime.datetime.now()
+        
+        # Start/Kill hyprsunset
+        hyprsunset_running = bool(int(subprocess.check_output("pgrep -x hyprsunset > /dev/null && echo 1 || echo 0", shell=True)))
+        if not ("07:00" < now.strftime("%H:%M") < "20:00"):
+            if not hyprsunset_running:
+                subprocess.Popen("hyprsunset --temperature 3500", shell=True)
+        else:
+            if hyprsunset_running:
+                subprocess.Popen("pkill hyprsunset", shell=True)
+
+
+        time.sleep(30)
+
